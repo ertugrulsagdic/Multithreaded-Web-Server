b51b5f940b8f75f4c0a3f897c1789ee1ae90efa3
ertugrulsagdic <38252389+ertugrulsagdic@users.noreply.github.com>
2021-01-04 23:23:12 +0300

    request headers error detections

--- server.py
+++ server.py
@@ -14,48 +14,83 @@ def thread_function(socket, address):
 
         # splits the request into part so that we can get the size of the file
         splitted_request = request.split()
-        command = splitted_request[0]
+        if splitted_request[0]:
+            command = splitted_request[0]
+        print("command ", command)
         # file size
-        file_size = splitted_request[1]
-        print(file_size[1:])
-        file_size = int(file_size[1:])
+        if len(splitted_request) >= 2:
+            # todo error burda integer olup olmadigini kontrol etmemiz lazim
+            try:
+                file_size = int(splitted_request[1])
+                print(file_size[1:])
+                file_size = int(file_size[1:])
+                print(b'file size ok')
+            except:
+                socket.send(b'400 Bad Request')
+                socket.send(b'Please enter integer value')
+                print(b'400 Bad Request')
+                socket.close()
+                return
 
-        if file_size < 100 or file_size > 20000:
-            socket.send(b'400 Bad Request aaa')
+        if command.upper() == b'HEAD' \
+                or command.upper() == b'POST' \
+                or command.upper() == b'PUT' \
+                or command.upper() == b'DELETE' \
+                or command.upper() == b'CONNECT' \
+                or command.upper() == b'OPTIONS' \
+                or command.upper() == b'TRACE':
+            socket.send(b'501 Not implemented')
+            print(b'501 Not implemented')
+            socket.close()
+        elif command.upper() == b'GET':
+            if file_size < 100 or file_size > 20000:
+                socket.send(b'400 Bad Request aaa')
+                print(b'400 Bad Request')
+                socket.close()
+            else:
+                # checks all the files if there is a file with given size
+                for file_name in os.listdir('./html'):
+                    # gets the file path
+                    path_to_file = os.getcwd() + '\\html\\' + file_name
+                    st = os.stat(path_to_file)
+                    print(file_size, file_name, st.st_size)
+                    # checks if the file size is equals
+                    if st.st_size == file_size:
+                        # opens the file and reads the content in it
+                        f = open(path_to_file)
+                        response_content = f.read()
+                        f.close()
+                        response_headers = {
+                            'Content-Type': 'text/html; encoding=utf8',
+                            'Content-Length': len(response_content),
+                        }
+                        response_headers_raw = ''.join('%s: %s\n' % (k, v) for k, v in response_headers.items())
+
+                        # reply as HTTP/1.1 server, saying "HTTP OK" (code 200)
+                        socket.send(b'HTTP/1.1 200 OK')
+                        # send response
+                        socket.send(response_headers_raw.encode())
+                        # send response content
+                        socket.send(response_content.encode())
+                        print('{} sended. File size : {}'.format(file_name, file_size))
+                        break
+                socket.close()
+        else:
+            socket.send(b'400 Bad Request')
             print(b'400 Bad Request')
             socket.close()
-        else:
-            # checks all the files if there is a file with given size
-            for file_name in os.listdir('./html'):
-                # gets the file path
-                path_to_file = os.getcwd() + '\\html\\' + file_name
-                st = os.stat(path_to_file)
-                print(file_size, file_name, st.st_size)
-                # checks if the file size is equals
-                if st.st_size == file_size:
-                    # opens the file and reads the content in it
-                    f = open(path_to_file)
-                    content = f.read()
-                    f.close()
-                    # creates a response with response message OK and content
-                    response = 'HTTP/1.1 200 OK\n\n' + content
-                    # sends the message and content
-                    socket.sendall(response.encode())
-                    print('{} sended. File_size : {}'.format(file_name, file_size))
-                    break
-            socket.close()
-    # if any errors caught it also controls if the file size is int or not
+    # if any errors caught
     except error:
-        print(error)
         socket.send(b'400 Bad Request')
-        print(b'bbbbb')
         print(b'400 Bad Request')
+        print(error)
         socket.close()
 
 
 IP = 'localhost'
 # port number
 PORT = 8080
+# int(sys.argv[1])
 
 # initialize socket
 server_socket = socket(AF_INET, SOCK_STREAM)
@@ -75,7 +110,7 @@ server_socket.listen(1)
 print('Server is ready to receive')
 
 while True:
-    # accepting the incomint request
+    # accepting the incoming request
     connection_socket, address = server_socket.accept()
     print('Connection\nIP Address and port: {}:{}\n{}\nSocket Protocol: {} Socket Family: {} Socket Type: {}'.format(
         address[0], address[1], connection_socket, connection_socket.proto, connection_socket.family,
