7b8e4ed7d1a840a940e4ae2a8ebae75ab7e48e4b
Göksel Tokur <gokseltokur@gmail.com>
2021-01-05 20:18:09 +0300

    proxy clien port

--- client.py
+++ client.py
@@ -3,7 +3,7 @@ import time
 
 # port number
 server_name = 'localhost'
-server_port = 8080
+server_port = 8888
 # connection to server
 client_socket = socket(AF_INET, SOCK_STREAM)
 client_socket.connect((server_name, server_port))
